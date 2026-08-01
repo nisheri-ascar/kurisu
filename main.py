d1ec19c2d8376500e620e233449d51e59a1a505e
nisheri-ascar <racsa.kristofer08@gmail.com>
2026-08-01 13:35:36 +0800

    minimal dashboard

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import discord
 import dotenv
 import os
+import asyncio
+
+time_phase0 = 0
+time_phase1 = 0
+time_phase2 = 0
+
+start_dashboard = f"""
+
+Currently Status
+**Phase 0** Checking if self is reachable: ... {time_phase0}
+**Phase 1** Checking if Remote Shell is reachable: ... {time_phase1}
+**Phase 2** Checking if Public IP is reachable: ... {time_phase2}
+                    """
+
 
 dotenv.load_dotenv()
 priv_token = str(os.getenv("TOKEN"))
@@ -17,18 +31,21 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+    print(f"{message.author} | {message.content}")
+    #if message.author == client.user:
+     #   return
 
-    if message.content.startswith("$kurisu"):
-        await message.channel.send("Run `$kurisu help` for more info")
-    elif message.content.startswith("$kurisu server start"):
-        await message_channel.send("""
-            Phase 0: Checking if self is up...
-            Phase 1: Testing remote shell is accessible.
-            Phase 2: Testing if Public IP is acce
+    if message.content.startswith(".kurisu"):
+        print(message.content)
+        user_msg = message.content.split(" ")
+        print(user_msg)
+        if user_msg[1] == "help":
+            await message.channel.send("todo: help")
+        elif user_msg[1] == "server":
+            if user_msg[2] == "start":
+                bot_msg = await message.channel.send(start_dashboard)
+                asyncio.sleep(5)
+                await bot_msg.edit(content="content has been updated\n\n" + start_dashboard)
 
-
-            """)
 
 client.run(priv_token) 
