64be716cd086d7e3c260af88724080d23084cc49
nisheri-ascar <racsa.kristofer08@gmail.com>
2026-08-01 14:12:36 +0800

    phase 0

--- main.py
+++ main.py
@@ -2,23 +2,12 @@ import discord
 import dotenv
 import os
 import asyncio
+import httpx
 
-time_phase0 = 0
-time_phase1 = 0
-time_phase2 = 0
-
-start_dashboard = f"""
-
-Currently Status
-**Phase 0** Checking if self is reachable: ... {time_phase0}
-**Phase 1** Checking if Remote Shell is reachable: ... {time_phase1}
-**Phase 2** Checking if Public IP is reachable: ... {time_phase2}
-                    """
 
 
 dotenv.load_dotenv()
 priv_token = str(os.getenv("TOKEN"))
-print(priv_token)
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -43,9 +32,18 @@ async def on_message(message):
             await message.channel.send("todo: help")
         elif user_msg[1] == "server":
             if user_msg[2] == "start":
-                bot_msg = await message.channel.send(start_dashboard)
-                asyncio.sleep(5)
-                await bot_msg.edit(content="content has been updated\n\n" + start_dashboard)
+                # process of starting the server starts here
+                await message.channel.send("starting server!")
+                msg = await message.channel.send("phase 0: checking if i can access myself 🔶")
+                async with httpx.AsyncClient() as status:
+                    r = await status.get("https://www.google.com")
+                    print(r.status_code)
+                    if r.status_code == 200:
+                        print("success")
+                        await msg.edit(content="phase 0: okay, i can access myself 🟢")
+
+
+
 
 
 client.run(priv_token) 
