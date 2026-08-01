3ead016212191149b97997d7f5de06b66bc07cd1
nisheri-ascar <racsa.kristofer08@gmail.com>
2026-08-01 19:38:00 +0800

    a

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import dotenv
 import os
 import asyncio
 import httpx
-import mcstatus
+from mcstatus import JavaServer
 import sys
+import subprocess
 
 dotenv.load_dotenv()
 PRIV_TOKEN = str(os.getenv("TOKEN"))
 HOSTED_LINK = str(os.getenv("HOSTED_LINK"))
-IP_ADDR = "mc.hypixel.net" # FIXME: make this on .env file.
+IP_ADDR = str(os.getenv("MC_IP_ADDR")) # FIXME: make this on .env file.
 
 
 intents = discord.Intents.default()
@@ -24,17 +25,20 @@ def check_variables():
     if HOSTED_LINK == "":
         print("HOSTED_LINK empty!")
         sys.exit(1)
-    if IP_ADDR == "":
-        print("IP_ADDR emtpy!")
+    if MC_IP_ADDR == "":
+        print("MC_IP_ADDR emtpy!")
         sys.exit(1)
 
 def check_server_status():
-    # this is syncronous code, pls check later if this actually works inside async
     try:
         server = JavaServer.lookup(IP_ADDR)
         status = server.status()
+        print(status.players.online)
     except:
         print("cannot access server!!")
+        return 1
+    else:
+        return 0
 
 @client.event
 async def on_ready():
@@ -56,9 +60,17 @@ async def on_message(message):
             if user_msg[2] == "start":
                 # process of starting the server starts here
                 await message.channel.send("starting server!")
-                # phase 0 self check
+                try:
+                    subprocess.run(["bash"], ["../run-server.sh"])
+                except:
+                    await message.channel.send("phase 0: failed to run start script!")
+                else:
+                    await message.channel.send("phase 0: succesfully started script!")
+
                 
-                msg = await message.channel.send("phase 0: checking if i can access myself 🔶")
+                # phase 1 self check
+                
+                msg = await message.channel.send("phase 1: checking if i can access myself 🔶")
                 async with httpx.AsyncClient() as status:
                     await asyncio.sleep(1)
                     try:
@@ -68,10 +80,10 @@ async def on_message(message):
                     else:
                         await msg.edit(content="phase 0: okay, i can access myself 🟢")
 
-                #phase 1 remote shell
-                msg = await message.channel.send("phase 1: checking if i can access the remote shell 🔶")
+                #phase 2 remote shell
+                msg = await message.channel.send("phase 2: checking if i can access the remote shell 🔶")
                 async with httpx.AsyncClient() as status:
-                    await asyncio.sleep(1) # change me to 60 later
+                    await asyncio.sleep(1) # FIXME: change me to 60 later
                     try:
                         r = await status.get("http://localhost:8080")
                     except:
@@ -79,6 +91,13 @@ async def on_message(message):
                     else:
                         print("success")
                         await msg.edit(content="phase 1: okay, i can access the remote shell 🟢")
+
+                # phase 3: checking if the server is up
+                msg = await message.channel.send("phase 3: checking if i can access the Public Minecraft Server 🔶")
+                if check_server_status() == 0:
+                    await msg.edit("phase 3: yes, i can access the minecraft server")
+                else:
+                    await msg.edit("phase 3: no, i cannot access the minecraft server")
 
 
 
