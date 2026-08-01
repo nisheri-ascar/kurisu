40988e2314956084aa6cf18c64183f2f1867cea5
nisheri-ascar <racsa.kristofer08@gmail.com>
2026-08-01 14:41:39 +0800

    phase 1

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ import os
 import asyncio
 import httpx
 
-
-
 dotenv.load_dotenv()
-priv_token = str(os.getenv("TOKEN"))
+PRIV_TOKEN = str(os.getenv("TOKEN"))
+HOSTED_LINK = str(os.getenv("HOSTED_LINK"))
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -34,6 +33,7 @@ async def on_message(message):
             if user_msg[2] == "start":
                 # process of starting the server starts here
                 await message.channel.send("starting server!")
+                # phase 0 self check
                 msg = await message.channel.send("phase 0: checking if i can access myself 🔶")
                 async with httpx.AsyncClient() as status:
                     r = await status.get("https://www.google.com")
@@ -41,9 +41,25 @@ async def on_message(message):
                     if r.status_code == 200:
                         print("success")
                         await msg.edit(content="phase 0: okay, i can access myself 🟢")
+                    else:
+                        print(f"error {r.status_code}")
+
+
+                #phase 1 remote shell
+                msg = await message.channel.send("phase 1: checking if i can access the remote shell 🔶")
+                async with httpx.AsyncClient() as status:
+                    r = await status.get("http://localhost:8080")
+                    print(r.status_code)
+                    if r.status_code == 200:
+                        print("success")
+                        await msg.edit(content="phase 1: okay, i can access the remote shell 🟢")
+                    else:
+                        print(f"error {r.status_code}")
+                        await msg.edit(content="phase 1: ERROR, I can't access the remote shell! ❗❗❗")
 
 
 
 
 
-client.run(priv_token) 
+
+client.run(PRIV_TOKEN) 
