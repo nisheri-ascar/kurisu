f2e4f328dcbb0eb65dcaadc3434372a48a32ccf3
nisheri-ascar <racsa.kristofer08@gmail.com>
2026-08-30 11:42:05 +0800

    minimally working v2 kurisu

--- main.py
+++ main.py
@@ -43,89 +43,69 @@ except FileNotFoundError as err:
 print(f"Crossed World Line at {datetime.now().strftime("%Y-%m-%d %H:%M:%S")} with commit {commit}")
 
 intents = discord.Intents.default()
-bot = commands.Bot(command_prefix="/", intent=intents)
+bot = commands.Bot(command_prefix=".", intents=intents, debug_guilds=[GUILD_ID])
 
-@bot.tree.command(name="start", description="Start the Minecraft server!")
-async def start(interaction: discord.Interaction):
-    await interaction.response.defer()
-    await interaction.followup.send("hello world from newer framework!!")
+@bot.slash_command(name="ping", description="Check kurisu's latency")
+async def ping(ctx: discord.Interaction):
+    await ctx.channel.send(f"pong! took {bot.latency} to process!")
+
+
+@bot.slash_command(name="start", description="Start the Minecraft Server!")
+async def start(ctx: discord.Interaction):
+    await ctx.response.defer()
+    await ctx.followup.send("**Server started by {message.author} at {now.strftime(\"%Y-%m-%d %H:%M:%S\")}**\n-# dev commit: `{commit}`") # temporarily remove format for now
+    await ctx.channel.send("-# This instance of kurisu environment is running locally.")
+    if PRODUCTION_MODE == False:
+        pass
+    try:
+        PHASE_LEVEL = 0
+        if DRY_RUN != True:
+            threading.Thread(target=start_script, daemon=True).start()
+            # FIXME: when implementing stop command, please make this have some flags.
+            # FIXME: add some guard check if the server is started multiple times OR server is in process of starting
+        else:
+            await ctx.channel.send(f"{b}DRY_RUN is set to True. Waiting time will be skipped and script will not be ran.{b}")
+    except:
+        await ctx.channel.send(f"{b}{PHASE_ERROR}{PHASE_LEVEL}{b}: Failed to run start script!")
+    else:
+        await ctx.channel.send(f"{b}{PHASE_DONE}{PHASE_LEVEL}{b}: Successfully started script!")
+
+    #phase 1
+    PHASE_LEVEL = 1
+    msg = await ctx.channel.send(f"{b}{PHASE_PROGRESS}{PHASE_LEVEL}{b}: checking if i can access the remote shell *({BASE_TIME_WAIT}s)*")
+    await asyncio.sleep(BASE_TIME_WAIT*2)
+    async with httpx.AsyncClient() as status:
+        try:
+            r = await status.get("http://127.0.0.1:6969")
+        except:
+            await msg.edit(content=f"{b}{PHASE_ERROR}{PHASE_LEVEL}{b}: Cannot access Remote Shell!")
+        else:
+            print("success")
+            await msg.edit(content=f"{b}{PHASE_DONE}{PHASE_LEVEL}{b}: Cloud Server is accessable")
+
+    # phase 2
+    PHASE_LEVEL = 2
+    msg = await ctx.channel.send(f"{b}{PHASE_PROGRESS}{PHASE_LEVEL}{b}: Checking Public IP Minecraft Server *({BASE_TIME_WAIT*3}s)* ")
+    await asyncio.sleep(BASE_TIME_WAIT*3)
+    if check_server_status() == 0:
+        await msg.edit(f"{b}{PHASE_DONE}{PHASE_LEVEL}{b}: Minecraft Server is accessable ")
+        await ctx.channel.send("**note:** server stops after 3 minutes of no players! be sure to join immediately!")
+    else:
+        await msg.edit(f"{b}{PHASE_ERROR}{PHASE_LEVEL}{b}: Minecraft Server is down! Is proxy down? ")
+
+@bot.slash_command(name="check", description="Check minecraft server\'s status")
+async def check(ctx: discord.Interaction):
+    if check_server_status() == 0:
+        await ctx.channel.send(f"checking: Minecraft Server is accessable ")
+    else:
+        await ctx.channel.send(f"checking: Minecraft Server is down! Is proxy down? ")
+        await ctx.channel.send("-# This instance of kurisu environment is running locally.")
 
 @bot.event
 async def on_ready():
     print(f"i am {bot.user}")
     guild = discord.Object(id=GUILD_ID)
-    await bot.tree.sync(guild=guild)
     threading.Thread(target=http_server, args=(HTTP_PORT,), daemon=True).start()
-
-@bot.event
-async def on_message(message):
-    print(f"{message.author} | {message.content}")
-    #if message.author == client.user:
-     #   return
-
-    if message.content.startswith(COMMAND_PREFIX):
-        print(message.content)
-        user_msg = message.content.split(" ")
-        print(user_msg)
-        if user_msg[1] == "help":
-            commands.help(message)
-        elif user_msg[1] == "ping":
-            commands.ping(message)
-        elif user_msg[1] == "server":
-            if user_msg[2] == "start":
-            # FIXME: move around this one on its own function.
-                # process of starting the server starts here
-                now = datetime.now()
-                await message.channel.send(f"**Server started by {message.author} at {now.strftime("%Y-%m-%d %H:%M:%S")}**\n-# dev commit: `{commit}`")
-                if PRODUCTION_MODE == False:
-                    pass
-                try:
-                    PHASE_LEVEL = 0
-                    if DRY_RUN != True:
-                        threading.Thread(target=start_script, daemon=True).start()
-                        # FIXME: when implementing stop command, please make this have some flags.
-                        # FIXME: add some guard check if the server is started multiple times OR server is in process of starting
-                    else:
-                        await message.channel.send(f"{b}DRY_RUN is set to True. Waiting time will be skipped and script will not be ran.{b}")
-                except:
-                    await message.channel.send(f"{b}{PHASE_ERROR}{PHASE_LEVEL}{b}: Failed to run start script!")
-                else:
-                    await message.channel.send(f"{b}{PHASE_DONE}{PHASE_LEVEL}{b}: Successfully started script!")
-
-                #phase 1
-                PHASE_LEVEL = 1
-                msg = await message.channel.send(f"{b}{PHASE_PROGRESS}{PHASE_LEVEL}{b}: checking if i can access the remote shell *({BASE_TIME_WAIT}s)*")
-                await asyncio.sleep(BASE_TIME_WAIT*2)
-                async with httpx.AsyncClient() as status:
-                    try:
-                        r = await status.get("http://127.0.0.1:6969")
-                    except:
-                        await msg.edit(content=f"{b}{PHASE_ERROR}{PHASE_LEVEL}{b}: Cannot access Remote Shell!")
-                    else:
-                        print("success")
-                        await msg.edit(content=f"{b}{PHASE_DONE}{PHASE_LEVEL}{b}: Cloud Server is accessable")
-
-                # phase 2
-                PHASE_LEVEL = 2
-                msg = await message.channel.send(f"{b}{PHASE_PROGRESS}{PHASE_LEVEL}{b}: Checking Public IP Minecraft Server *({BASE_TIME_WAIT*3}s)* ")
-                await asyncio.sleep(BASE_TIME_WAIT*3)
-                if check_server_status() == 0:
-                    await msg.edit(f"{b}{PHASE_DONE}{PHASE_LEVEL}{b}: Minecraft Server is accessable ")
-                    await message.channel.send("**note:** server stops after 3 minutes of no players! be sure to join immediately!")
-                else:
-                    await msg.edit(f"{b}{PHASE_ERROR}{PHASE_LEVEL}{b}: Minecraft Server is down! Is proxy down? ")
-
-            elif user_msg[2] == "check":
-                # FIXME: Dont Repeat Yourself!
-                if check_server_status() == 0:
-                    await message.channel.send(f"checking: Minecraft Server is accessable ")
-                else:
-                    await message.channel.send(f"checking: Minecraft Server is down! Is proxy down? ")
-                    await message.channel.send("-# This instance of kurisu environment is running locally.")
-            elif user_msg[2] == "stop":
-                await message.channel.send("not implemented **(yet!)**")
-
-
 
 bot.run(PRIV_TOKEN)
 
